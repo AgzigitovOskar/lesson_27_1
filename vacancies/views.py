ccdb54325af58485ac479c937866abe2265c8c6b
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-01-26 21:41:39 +0300

    28_2 vid_3

--- vacancies/views.py
+++ vacancies/views.py
@@ -28,30 +28,30 @@ class VacancyListView(ListView):
 
         self.object_list = self.object_list.order_by("text")
 
-        response =[]
-        for vacancy in self.object_list:
-            response.append({
+        # response =[]
+        # for vacancy in self.object_list:
+        #     response.append({
+        #         "id": vacancy.id,
+        #         "text": vacancy.text
+        #     })
+
+        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        vacancies = []
+        for vacancy in page_obj:
+            vacancies.append({
                 "id": vacancy.id,
                 "text": vacancy.text
             })
 
-            # paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
-            # page_number = request.GET.get("page")
-            # page_obj = paginator.get_page(page_number)
-            #
-            # vacancies = []
-            # for vacancy in page_obj:
-            #     vacancies.append({
-            #         "id": vacancy.id,
-            #         "text": vacancy.text
-            #     })
-            #
-            # response = {
-            #     "items": vacancies,
-            #     "num_pages": paginator.num_pages,
-            #     "total": paginator.count
-            # }
-            
+        response = {
+            "items": vacancies,
+            "num_pages": paginator.num_pages,
+            "total": paginator.count
+        }
+
         return JsonResponse(response, safe=False)
 
 
