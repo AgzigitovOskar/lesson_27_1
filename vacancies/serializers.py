0baf710bf19a6180113b5146d9cbdf125f444111
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-01-31 22:26:39 +0300

    29_1 vid_6

--- vacancies/serializers.py
+++ vacancies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from vacancies.models import Vacancy
+from vacancies.models import Vacancy, Skill
 
 
 # Сериализаторы модели
@@ -31,18 +31,64 @@ class VacancyDetailSerializer(serializers.ModelSerializer):
 
 class VacancyCreateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
+    skills = serializers.SlugRelatedField(
+        required=False,
+        many=True,
+        queryset=Skill.objects.all(),
+        slug_field="name"
+    )
 
     class Meta:
         model = Vacancy
-        exclude = ["skills"]
+        fields = '__all__'
+
+    def is_valid(self, *, raise_exception=False):
+        self._skills = self.initial_data.pop("skills")
+        return super().is_valid(raise_exception=raise_exception)
+
+    def create(self, validated_data):
+        vacancy = Vacancy.objects.create(**validated_data)
+
+        for skill in self._skills:
+            skill_obj, _= Skill.objects.get_or_create(name=skill)
+            vacancy.skills.add(skill_obj)
+
+        vacancy.save()
+        return vacancy
 
 
-# Простые сериализаторы
-# class VacancySerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     text = serializers.CharField(max_length=2000)
-#     slug = serializers.CharField(max_length=50)
-#     status = serializers.CharField(max_length=6)
-#     created = serializers.DateTimeField()
-#     username = serializers.CharField(max_length=100)
+class VacancyUpdateSerializer(serializers.ModelSerializer):
+    skills = serializers.SlugRelatedField(
+        required=False,
+        many=True,
+        queryset=Skill.objects.all(),
+        slug_field="name"
+    )
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    created = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Vacancy
+        fields = ["id", "text", "status", "slug", "user", "created", "skills"]
+
+    def is_valid(self, *, raise_exception=False):
+        self._skills = self.initial_data.pop("skills")
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self):
+        vacancy = super().save()
+
+        for skill in self._skills:
+            skill_obj, _= Skill.objects.get_or_create(name=skill)
+            vacancy.skills.add(skill_obj)
+
+        vacancy.save()
+        return vacancy
+
+
+class VacancyDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ["id"]
+
 
